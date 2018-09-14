66dd3fc8d58295cdc03046486a14c1736299981c
MatthewDodi <matthew.dodi@gmail.com>
2018-09-14 02:22:15 -0700

    implemented a dispatch method to increment counter

--- src/store/reducer.js
+++ src/store/reducer.js
@@ -5,6 +5,12 @@ const initialState = {
 }
 
 const reducer = (state = initialState, action) => {
+    if (action.type === 'INCREMENT') {
+        return {
+            ...state,
+            counter: state.counter + 1
+        }
+    }
     return state;
 }
 
