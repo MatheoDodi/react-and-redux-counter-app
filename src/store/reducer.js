47d7f4dd840efb2a64aeceaf47ef296d6b832a7c
MatthewDodi <matthew.dodi@gmail.com>
2018-09-16 20:02:12 -0700

    outsourced the actionTypes in to an external file

--- src/store/reducer.js
+++ src/store/reducer.js
@@ -1,3 +1,5 @@
+import * as actionTypes from './actions';
+
 const initialState = {
     counter: 0,
     results: []
@@ -5,17 +7,17 @@ const initialState = {
 
 const reducer = (state = initialState, action) => {
     switch (action.type) {
-        case 'INCREMENT':
+        case actionTypes.INCREMENT:
             return {  ...state, counter: state.counter + 1 }
-        case 'DECREMENT':
+        case actionTypes.DECREMENT:
             return {  ...state, counter: state.counter - 1 }
-        case 'ADD':
+        case actionTypes.ADD:
             return {  ...state, counter: state.counter + action.value }
-        case 'SUB':
+        case actionTypes.SUB:
             return {  ...state, counter: state.counter - action.value }
-        case 'STORE_RESULT':
+        case actionTypes.STORE_RESULT:
             return { ...state, results: state.results.concat({val : state.counter, id: new Date()}) }
-        case 'DELETE_RESULT':
+        case actionTypes.DELETE_RESULT:
             const newResults = state.results.filter(elem => elem.id !== action.elementId);
             return { ...state, results: newResults}
         default:
